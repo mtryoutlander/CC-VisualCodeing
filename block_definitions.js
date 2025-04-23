415af85c98f6739f60ea4c37b380de70e2a52f08
mtryoutlander <32582120+mtryoutlander@users.noreply.github.com>
2025-04-23 09:00:49 -0500

    Create block_definitions.js
    custom blocks

---
+++ block_definitions.js
@@ -0,0 +1,11 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    type: "turtle_forward",
+    message0: "move turtle forward",
+    previousStatement: null,
+    nextStatement: null,
+    colour: 160,
+    tooltip: "Moves the turtle forward",
+    helpUrl: ""
+  }
+]);
