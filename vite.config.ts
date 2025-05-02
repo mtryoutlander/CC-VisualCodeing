55016763052353dec6dc6b8a2a1e6ab8c5a0e6c3
mtryoutlander <32582120+mtryoutlander@users.noreply.github.com>
2025-05-02 05:19:35 -0500

    getting tiered of comments durning testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/CC-VisuralCodeing/', // Ensure the base path matches your repository name
-  build: {
-    rollupOptions: {
-      input: './src/main.ts', // Explicitly set the entry point
-    },
-  },
+  
 });
